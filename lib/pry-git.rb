292f7daf1d5b7fa97e775b3f7ab5719031b28ec3
John Mair <jrmair@gmail.com>
2011-07-15 20:45:55 +1200

    upgraded all git related commands and added 'git add'

--- lib/pry-git.rb
+++ lib/pry-git.rb
@@ -3,68 +3,96 @@
 
 require "pry-git/version"
 require "pry"
+require 'grit'
+require 'diffy'
+require 'tempfile'
 
 module PryGit
   GitCommands = Pry::CommandSet.new do
-    command "blame", "Show blame for a method", :requires_gem => "grit" do |meth_name|
-      require 'grit'
-      if (meth = get_method_object(meth_name, target, {})).nil?
-        output.puts "Invalid method name: #{meth_name}."
-        next
+    command "git blame", "Show blame for a method" do |meth_name|
+        if (meth = get_method_object(meth_name, target, {})).nil?
+          output.puts "Invalid method name: #{meth_name}."
+          next
+        end
+
+        file_name = meth.source_location.first
+        code, start_line = method_code_from_head(meth)
+
+        repo ||= Grit::Repo.new(".")
+        num_lines = code.lines.count
+        authors = repo.blame(file_name, repo.head.commit).lines.select do |v|
+          v.lineno >= start_line && v.lineno <= start_line + num_lines
+        end.map do |v|
+          v.commit.author.output(Time.new).split(/</).first.strip
+        end
+
+        lines_with_blame = []
+        code.lines.zip(authors) { |line, author| lines_with_blame << ("#{author}".ljust(10) + colorize_code(line)) }
+        output.puts lines_with_blame.join
       end
 
-      repo ||= Grit::Repo.new(Dir.pwd)
-      start_line = meth.source_location.last
-      num_lines = meth.source.lines.count
-      authors = repo.blame(meth.source_location.first).lines.select do |v|
-        v.lineno >= start_line && v.lineno <= start_line + num_lines
-      end.map do |v|
-        v.commit.author.output(Time.new).split(/</).first.strip
+      command "git diff", "Show the diff for a method" do |meth_name|
+
+        if (meth = get_method_object(meth_name, target, {})).nil?
+          output.puts "Invalid method name: #{meth_name}."
+          next
+        end
+
+        output.puts colorize_code(Diffy::Diff.new(method_code_from_head(meth).first, meth.source))
       end
 
-      lines_with_blame = []
-      meth.source.lines.zip(authors) { |line, author| lines_with_blame << ("#{author}".ljust(10) + colorize_code(line)) }
-      output.puts        lines_with_blame.join
-    end
+      command "git add", "Add a method to index" do |meth_name|
+        if (meth = get_method_object(meth_name, target, {})).nil?
+          output.puts "Invalid method name: #{meth_name}."
+          next
+        end
+        repo = Grit::Repo.new('.')
 
-    command "diff", "Show the diff for a method", :requires_gem => ["grit", "diffy"] do |meth_name|
-      require 'grit'
-      require 'diffy'
+        file_name = relative_path(meth.source_location.first)
+        file_data = get_file_from_commit(file_name)
+        code, start_line = method_code_from_head(meth)
+        end_line = start_line + code.lines.count
 
-      if (meth = get_method_object(meth_name, target, {})).nil?
-        output.puts "Invalid method name: #{meth_name}."
-        next
+        before_code = file_data.lines.to_a[0..(start_line - 2)]
+        after_code = file_data.lines.to_a[end_line - 1..-1]
+
+        final_code = before_code << meth.source.lines.to_a << after_code
+
+        t = Tempfile.new("tmp")
+        t.write final_code.join
+        t.close
+
+        sha1 = `git hash-object -w #{t.path}`.chomp
+        system("git update-index --cacheinfo 100644 #{sha1} #{file_name}")
       end
 
-      output.puts colorize_code(Diffy::Diff.new(method_code_from_head(meth), meth.source))
-    end
+      helpers do
+        def get_file_from_commit(path)
+          repo = Grit::Repo.new('.')
+          head = repo.commits.first
+          tree_names = path.split("/")
+          start_tree = head.tree
+          blob_name = tree_names.last
+          tree = tree_names[0..-2].inject(start_tree)  { |a, v|  a.trees.find { |t| t.basename == v } }
+          blob = tree.blobs.find { |v| v.basename == blob_name }
+          blob.data
+        end
 
-    helpers do
-      def get_file_from_commit(path)
-        repo = Grit::Repo.new(Dir.pwd)
-        head = repo.commits.first
-        tree_names = path.split("/")
-        start_tree = head.tree
-        blob_name = tree_names.last
-        tree = tree_names[0..-2].inject(start_tree)  { |a, v|  a.trees.find { |tree| tree.basename == v } }
-        blob = tree.blobs.find { |v| v.basename == blob_name }
-        blob.data
+        def method_code_from_head(meth)
+          code = get_file_from_commit(relative_path(meth.source_location.first))
+          search_line = meth.source.lines.first.strip
+          _, start_line = code.lines.to_a.each_with_index.find { |v, i| v.strip == search_line }
+          start_line
+          [Pry.new(:input => StringIO.new(code.lines.to_a[start_line..-1].join)).r(target), start_line + 1]
+        end
+
+        def relative_path(path)
+          path =~ /#{Regexp.escape(File.expand_path("."))}\/(.*)/
+          $1
+        end
+
       end
-
-      def method_code_from_head(meth)
-        rel_path = relative_path(meth.source_location.first)
-        code = get_file_from_commit(rel_path)
-        start_line = meth.source_location.last
-        code_length = meth.source.lines.count
-        code.lines.to_a[(start_line - 1)...((start_line -1) + code_length)].join
-      end
-
-      def relative_path(path)
-        path =~ /#{Regexp.escape(File.expand_path(Dir.pwd))}\/(.*)/
-        $1
-      end
-    end
-
+ 
   end
 end
 
